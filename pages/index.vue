8ac7ff7e3b22f28817db75f19c9b3c8af0bdf844
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-01-17 22:35:54 +0300

    feature: create ability to add workspace with enter

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
-<script setup>
+<script lang="ts" setup>
 import { ref } from 'vue';
+
+interface Workspace {
+  id: string;
+  name: string;
+}
+
 const newWorkspaceName = ref('');
-const workspaceList = ref([]);
+const workspaceList = ref<Workspace[]>([]);
 const createWorkspace = () => {
   const newWorkspace = {
-    id: Math.floor(Math.random() * 100),
+    id: Math.floor(Math.random() * 100).toString(),
     name: newWorkspaceName.value,
   };
   workspaceList.value.push(newWorkspace);
@@ -17,10 +23,10 @@ const createWorkspace = () => {
     <h1>Home Page</h1>
     <h2>Recently Viewed</h2>
     <h2>Workspaces</h2>
-    <input type="text" v-model="newWorkspaceName" />
+    <input type="text" v-model="newWorkspaceName" @keyup.enter="createWorkspace" />
     <button @click="createWorkspace">Create Workspace</button>
     <ul>
-      <li v-for="workspace in workspaceList" :key="workspace.id" class="workspace-card">
+      <li v-for="workspace in workspaceList" :key="workspace.id.toString()" class="workspace-card">
         {{ workspace.id }}: {{ workspace.name }}
       </li>
     </ul>
